738158f67664e77928ab40f2dbca4416f6ee053b
Victor <vifeanyi33@gmail.com>
2018-02-01 02:51:53 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  form: FormGroup;
+
   title = 'app';
+
+  constructor(public formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+
+        firstname: ['', [ Validators.required, Validators.maxLength(12)] ],
+        lastname: ['', Validators.required],
+        email: ['', Validators.required]
+
+    });
+
+  }
+
+  onFormSubmit() {
+
+    if(this.form.value.email == 'ng@gmail.com') {
+      console.log('Yup');
+    } else {
+      console.log('Nah...');
+    }
+
+    console.log(this.form.value);
+
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      ReactiveFormsModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
